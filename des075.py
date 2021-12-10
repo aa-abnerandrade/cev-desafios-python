befb6848a38886bf7414edb22fc3acb879b6602a
Abner Andrade <aa.abnerandrade@gmail.com>
2021-12-09 21:33:25 -0300

    16up2

--- des075.py
+++ des075.py
@@ -3,3 +3,39 @@ print('DESAFIO 75'.center(40))
 Ler 4 valores pelo teclado e guardar em uma tupla. Após isso, mostre:
 a) quantas vezes apareceu o valor 9 / b) em que posição foi digita o primeiro valor 3 / c) quais foram os números pares
 '''
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número:  '))
+n3 = int(input('Digite o terceiro número: '))
+n4 = int(input('Digite o último número: '))
+
+numbers = (n1, n2, n3, n4)
+print(numbers)
+
+print(numbers[2])
+print(numbers[3] % 2)
+
+pares = 0
+for cont in range(1, 4+1):
+    print(numbers[cont])
+    if (numbers[cont] % 2) == 0:
+        if pares == 0:
+            pares = numbers[cont]
+        else:
+            pares += numbers[cont]
+print(f'Os números pares são: {pares}')
+
+nrepeat = int(input('Qual número deseja contar: '))
+qtdrepeat = numbers.count(nrepeat)
+if qtdrepeat == 0:
+    print(f'O número {nrepeat} não apareceu na listagem. ')
+else:
+    print(f'O número {nrepeat} apareceu {qtdrepeat} vez(es). ')
+
+nsearch = int(input('Qual número deseja localizar: '))
+nposit = numbers.index(nsearch)
+if nposit == 0:
+    print(f'Não localizamos o número {nsearch} em nenhuma posição. ')
+else:
+    print(f'O número {nsearch} foi localizado na posição {nposit}')
+
+
--- des076.py
+++ des076.py
@@ -3,3 +3,10 @@ print('DESAFIO 76'.center(40))
 Criar tupla única com nomes de produtos com e seus respectivos preços
 No final, mostrar uma listagem de preços organizando os dados em forma tabular. Sem interação. 
 '''
+store = ('Notebook', 5449.00, 'Livro', 114,  'Headseat', 399, 'Mouse', 48, 'Webcam', 1445, 'Monitor', 1099, 'Caderno', 34.90, 'Caneta', 2, 'Casaco', 125)
+
+print(f" \n{'|':|^40} \n{'LISTAGEM DE PREÇOS': ^40} \n{'|':|^40}")
+
+for cont in range(0, len(store), 2):
+    print(f' {store[cont]:.<26} R$ {store[cont+1]:>8.2f}')
+print(f"{'|':|^40}")
